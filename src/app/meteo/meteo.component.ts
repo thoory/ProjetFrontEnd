5db476c35f70e164449cdac79214f50d32bd7093
tomtom.ory <tomtom.ory@gmail.com>
2018-04-14 03:36:25 -0400

    css

--- src/app/meteo/meteo.component.ts
+++ src/app/meteo/meteo.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { Town } from '../town'
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
 
 @Component({
   selector: 'app-meteo',
   templateUrl: './meteo.component.html',
-  styleUrls: ['./meteo.component.css']
+  styleUrls: ['./meteo.component.css'],
+  imports: [BrowserAnimationsModule],
+  imports: [MatButtonModule, MatCheckboxModule]
 })
 export class MeteoComponent implements OnInit {
 
